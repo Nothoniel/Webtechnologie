72664ed06562827bc9af2b2b1e7ab040d9561ed8
Toan39 <71543094+Toan39@users.noreply.github.com>
2021-03-16 21:06:38 +0100

    ColoringDone
    Coloring the webpage.

--- Opdracht 2/script.js
+++ Opdracht 2/script.js
@@ -1,6 +1,7 @@
 //declarations
 var options = ["body", "article", "header", "section", "aside", "footer"];
 var type = ["type", "class", "name"];
+var cssVar = ["--dark-1-header-color", "--dark-1-color", "--dark-2-footer-color", "--dark-2-color", "--dark-3-color", "--dark-4-color", "--dark-5-color","--dark-7-color"]
 
 //menu that changes the lay-out of a webpage and is present in the navbar
 //adding a new button to the navbar that shows the menu
@@ -98,19 +99,77 @@ function addInput(element, type, value) {
    }
  }
 
-//applying the new CSSrules
-function applyNewChanges(selected, collection, newColor, newFontsizepx) {    
-   var i;
-   for (i=0; i<collection.length; i++)
+ //hsl color
+ function colorBrightness(hexStr, lum) {
+	// hex string splitted up in rgb pairs, that are headecimal numbers
+	hexStr = hexStr.replace(/[^0-9a-f]/gi, "");
+
+   // converts hexadecimal number to decimal and change brightness
+	var rgb = "#", c, i;
+	for (i = 0; i < 3; i++) {
+		c = parseInt(hexStr.substr(i*2,2), 16);
+		c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
+		rgb += ("00"+c).substr(c.length);
+	}
+	return rgb;
+}
+
+function colorTheElement(newColor, selected) {
+   var p;
+   for(p=0; p<cssVar.length; p++)
    {
-      collection[i].style.fontSize = newFontsizepx;
-      if (selected !== "body")
-      {
-         collection[i].style.color = newColor;
+      if(selected == "section"){
+         if( cssVar[p] == "--dark-1-header-color") {
+            continue;
+         }
+      } 
+
+      if(cssVar[p] == "--dark-2-footer-color"){
+         continue;
       }
-      else
+      
+      var saturatedColor;
+      if(p<4){
+         saturatedColor= colorBrightness(newColor,p/2);
+      }
+      else{
+         saturatedColor= colorBrightness(newColor,-p/11);
+      }
+      document.querySelector(":root").style.setProperty(cssVar[p], saturatedColor);
+   }
+}
+
+function colorHyperlink(className) {
+   var k;
+   var link = document.getElementsByClassName(className);
+   for (k=0; k<link.length; k++){
+   link[k].style.color = colorBrightness(newColor, -0.1);
+   }
+}
+
+//applying the new CSSrules
+function applyNewChanges(selected, collection, newColor, newFontsizepx) {
+   if (selected == "article" || selected == "section") {
+      colorTheElement(newColor, selected);
+   }
+   else{    
+      var i;
+      for (i=0; i<collection.length; i++)
       {
-         collection[i].style.backgroundColor = newColor;
+         switch(selected) {
+            case "body":
+               collection[i].style.backgroundColor = newColor;   
+               break;
+            case "footer":
+               colorHyperlink("footer-mail");
+               break;
+            case "aside":
+               colorHyperlink("figure-link");
+               break;
+            default:
+               collection[i].style.fontSize = newFontsizepx;
+               collection[i].style.color = newColor;
+         }
       }
    }
 }
@@ -139,6 +198,6 @@ var sheet = (function() {
 })();
 
 //adding css-rules to the stylesheet
- sheet.insertRule('.menucontent { padding: 50px; display: none; position: absolute; background-color: #544E68; width:20%; z-index: 1; }', 0);
+ sheet.insertRule('.menucontent { padding: 50px; display: none; position: absolute; background-color: #544E68; color: #FFD4A3; width:20%; z-index: 1; }', 0); //Make inherit when coloring body
  sheet.insertRule('.dropdown:hover .menucontent { display: block; }', 1);
  sheet.insertRule('.selected { width: 300px; }', 2);
