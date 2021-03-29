a17ecaf8159d9cc9e7bd219762fb4cd0d027879a
Toan39 <71543094+Toan39@users.noreply.github.com>
2021-03-29 14:24:52 +0200

    changingPort
    Changing the port

--- Opdracht 3/myapp/index.js
+++ Opdracht 3/myapp/index.js
@@ -2,13 +2,12 @@ const express = require('express');
 
 const app = express();
 
-
 // this function is called every time an http request is received
 app.get ('/', function(req, res) { 
     res.send('Hello World');
 });
 
-//now app is running - listening to requests on port 8081 
-app.listen(8081, function(){
-    console.log('Server started on port 8081...');
+//now app is running - listening to requests on port 8046 
+app.listen(8046, function(){
+    console.log('Server started on port 8046...');
 });
