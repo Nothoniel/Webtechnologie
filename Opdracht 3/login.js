89ef077dac6604701c4a507a2d2a0ac4e0aa05e6
Nothoniel <nathanvdkolk@gmail.com>
2021-04-02 15:32:47 +0200

    vgm ben ik klaar misschien

--- Opdracht 3/login.js
+++ Opdracht 3/login.js
@@ -1,5 +1,3 @@
-var loginSection = document.querySelector(".webpage-content__section");
-
 class user
 {
     constructor(username, password, firstName, lastName)
@@ -42,7 +40,16 @@ class classRoom
     }
 }
 
-function createLoginPage() {
+function clearPage()
+{
+    var loginSection = document.querySelector(".webpage-content__section");
+    for (child of loginSection.childNodes) loginSection.removeChild(child);
+}
+
+function createLoginPage()
+{
+    clearPage();
+    var loginSection = document.querySelector(".webpage-content__section");
     var loginSubSection = document.createElement("section");
     loginSubSection.setAttribute("class", "webpage-content__section__subsection");
 
@@ -71,39 +78,96 @@ function createLoginPage() {
     loginSubSection.appendChild(document.createElement("br"));
 
     var submitButton = document.createElement("input");
-    submitButton.type = "button";
-    submitButton.value = "Log in";
+    submitButton.setAttribute("type", "button");
+    submitButton.setAttribute("value", "Log in");
     loginSubSection.appendChild(submitButton);
 
+    loginSubSection.appendChild(document.createElement("br"));
+    loginSubSection.appendChild(document.createElement("br"));
+
+    //dont have an account? - button
+    loginSubSection.appendChild(document.createTextNode("Don't have an account?"));
+    loginSubSection.appendChild(document.createElement("br"));
+
+    var RegisterButton = document.createElement("input");
+    RegisterButton.setAttribute("type", "button");
+    RegisterButton.setAttribute("value", "Register an account");
+    RegisterButton.addEventListener("click", function () { createRegisterPage() });
+    loginSubSection.appendChild(RegisterButton);
+
+    //add the login subsection to the screen
     loginSection.appendChild(loginSubSection);
 }
 
 function createRegisterPage()
 {   
+    clearPage();
+    var loginSection = document.querySelector(".webpage-content__section");
     var loginSubSection = document.createElement("section");
     loginSubSection.setAttribute("class", "webpage-content__section__subsection");
 
     var loginHeader = document.createElement("h1");
     loginHeader.appendChild(document.createTextNode("Register an account"));
+    loginSubSection.appendChild(loginHeader);
 
+    var backButton = document.createElement("input");
+    backButton.setAttribute("type", "button");
+    backButton.setAttribute("value", "Back to Log in");
+    backButton.addEventListener("click", function () { createLoginPage() });
+    loginSubSection.appendChild(backButton);
+
+    loginSubSection.appendChild(document.createElement("br"));
+    loginSubSection.appendChild(document.createElement("br"));
+
+    var usernameLabel = document.createElement("label");
+    usernameLabel.appendChild(document.createTextNode("username:"));
+    loginSubSection.appendChild(usernameLabel);
     var usernameInput = document.createElement("input");
     loginSubSection.appendChild(usernameInput);
 
+    loginSubSection.appendChild(document.createElement("br"));
+    loginSubSection.appendChild(document.createElement("br"));
+
+    var passwordLabel = document.createElement("label");
+    passwordLabel.appendChild(document.createTextNode("password:"));
+    loginSubSection.appendChild(passwordLabel);
     var passwordInput = document.createElement("input");
+    passwordInput.setAttribute("type", "password");
     loginSubSection.appendChild(passwordInput);
 
+    loginSubSection.appendChild(document.createElement("br"));
+
+    var confirmPasswordLabel = document.createElement("label");
+    loginSubSection.appendChild(confirmPasswordLabel);
+    confirmPasswordLabel.appendChild(document.createTextNode("confirm password:"));
     var confirmPassword = document.createElement("input");
+    confirmPassword.setAttribute("type", "password");
     loginSubSection.appendChild(confirmPassword);
 
+    loginSubSection.appendChild(document.createElement("br"));
+    loginSubSection.appendChild(document.createElement("br"));
+
+    var firstNameLabel = document.createElement("label");
+    firstNameLabel.appendChild(document.createTextNode("first name:"));
+    loginSubSection.appendChild(firstNameLabel);
     var firstNameInput = document.createElement("input");
     loginSubSection.appendChild(firstNameInput);
 
+    loginSubSection.appendChild(document.createElement("br"));
+    loginSubSection.appendChild(document.createElement("br"));
+
+    var lastNameLabel = document.createElement("label");
+    lastNameLabel.appendChild(document.createTextNode("last name:"));
+    loginSubSection.appendChild(lastNameLabel);
     var lastNameInput = document.createElement("input");
     loginSubSection.appendChild(lastNameInput);
 
+    loginSubSection.appendChild(document.createElement("br"));
+    loginSubSection.appendChild(document.createElement("br"));
+
     var submitButton = document.createElement("input");
-    submitButton.type = "button";
-    submitButton.value = "submit";
+    submitButton.setAttribute("type", "button");
+    submitButton.setAttribute("value", "submit");
     loginSubSection.appendChild(submitButton);
 
     loginSection.appendChild(loginSubSection);
