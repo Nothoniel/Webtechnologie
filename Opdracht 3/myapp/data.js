7ec393082faaa37efa7c9dc16909a8860c700936
Toan39 <71543094+Toan39@users.noreply.github.com>
2021-04-04 16:43:33 +0200

    dffs
    slicingproblem

--- Opdracht 3/myapp/data.js
+++ Opdracht 3/myapp/data.js
@@ -1,32 +1,42 @@
 function moduleDataTransmission(sql) {
+    var dataExport = [];
+
+    function test (data) {
+            dataExport = data.slice(); 
+            // console.log(data);    
+    }
+
     // opens the database
     const sqlite3 = require('sqlite3').verbose();
 
-    let db = new sqlite3.Database('./db/webtech.db', sqlite3.OPEN_READWRITE, (error) => {
-        if (error) {
-        console.error(error.message);
-        }
-        console.log('Connected to the webtech database.');
-    });
+    var readData = function(test){
+                        let db = new sqlite3.Database('./db/webtech.db', sqlite3.OPEN_READWRITE, (error) => {
+                            if (error) {
+                            console.error(error.message);
+                            }
+                            console.log('Connected to the webtech database.');
+                        });
 
-    //printing each row            
-    db.all(sql, [], (err, data) => {
-        if (err) {
-          throw err;
-        }
+                        //printing each row            
+                        db.all(sql, function (error, data) {
+                            if (error) {
+                                throw error;
+                            }
+                            test(data);
+                            db.close();
+                        });
 
-        console.log(data[0].username);
-        console.log(data[0].password);
-        module.exports = {data};
-        
-    });
+                            // db.close((error) => {
+                            //     if (error) {
+                            //     console.error(error.message);
+                            //     }
+                            //     console.log('Close the database connection.');
+                            // });
+    }
 
-    db.close((error) => {
-        if (error) {
-        console.error(error.message);
-        }
-        console.log('Close the database connection.');
-    });
+    readData(test);
+    console.log(dataExport[0]);
 }
+
 
 module.exports = moduleDataTransmission;
--- Opdracht 3/myapp/index.js
+++ Opdracht 3/myapp/index.js
@@ -2,12 +2,21 @@ const express = require('express');
 const app = express();
 var path = require('path');
 const bcrypt = require('bcrypt');
-const bodyParser = require('body-parser');
 var serveStatic = require('serve-static');
 var moduleDataTransmission = require('./data');
-const users = require('./data').data;
 
 const PORT=8046; 
+
+// app.use(urlencoded());
+
+// retrieving  files
+app.use(serveStatic(path.join(__dirname, 'public')))
+
+
+//called every time an http request is received, like a starting file can be set  
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'./public/index.html'));
+});
 
 //user sql query
 let query1 = `SELECT UserName username,
@@ -17,37 +26,31 @@ let query1 = `SELECT UserName username,
 //acces database               
 moduleDataTransmission(query1); //user system
 
-// retrieving  files
-app.use(serveStatic(path.join(__dirname, 'public')))
 
-//called every time an http request is received
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
 
 //athentication of user
-app.post('/loginpage_htmlFile', async (req, res) => {
-    try{
-        let foundUser = users.find((data) => req.body.username === data.username);
-        if (foundUser) {
+// app.post('/loginpage_htmlFile', async (req, res) => {
+//     try{
+//         let foundUser = users.find((data) => req.body.username === data.username);
+//         if (foundUser) {
     
-            let submittedPass = req.body.password; 
-            let storedPass = foundUser.password; 
+//             let submittedPass = req.body.password; 
+//             let storedPass = foundUser.password; 
     
-            const passwordMatch = await bcrypt.compare(submittedPass, storedPass);
-            if (passwordMatch) {
-                res.send(`matching password`);
-            } else {
-                res.send("not matching password");
-            }
-        }
-        else {
-            res.send("username does not exist");
-        }
-    } catch{
-        res.send("Internal server error");
-    }
-});
+//             const passwordMatch = await bcrypt.compare(submittedPass, storedPass);
+//             if (passwordMatch) {
+//                 res.send(`matching password`);
+//             } else {
+//                 res.send("not matching password");
+//             }
+//         }
+//         else {
+//             res.send("username does not exist");
+//         }
+//     } catch{
+//         res.send("Internal server error");
+//     }
+// });
 
 
 //now app is running - listening to requests on port 8046 
