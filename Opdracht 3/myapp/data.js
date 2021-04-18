d42fa884ab101cb8f8c3a7d4e3ea90db125eb57f
SayoranTheMagical <74265273+SayoranTheMagical@users.noreply.github.com>
2021-04-18 10:33:48 +0200

    few things changed

--- Opdracht 3/myapp/data.js
+++ Opdracht 3/myapp/data.js
@@ -7,21 +7,18 @@ function getData(sql) {
 
     return new Promise(function(resolve, reject) {
         //executing the sql-string            
-        db.all(sql, function (err, rows) {
-            if (err) {
+        db.all(sql, function(err, rows) {
+            if (err)
                 throw err;
-            }
-            else{
+            else
                 resolve(rows);
-            }
-            console.log("Execute sql statement");
+            console.log('Execute sql statement');
         }); 
 
         //closing the connection
         db.close((error) => {
-            if (error) {
-            console.error(error.message);
-            }
+            if (error)
+                console.error(error.message);
             console.log('Close the database connection.');
         }); 
     })
--- Opdracht 3/myapp/db.js
+++ Opdracht 3/myapp/db.js
@@ -4,9 +4,8 @@ function openDB() {
 
     //connectiong to sql
     let db = new sqlite3.Database('./db/webtech.db', sqlite3.OPEN_READWRITE, (error) => {
-    if (error) {
-        console.error(error.message);
-    }
+        if (error)
+            console.error(error.message);
         console.log('Connected to the webtech database.');
     });
     return db;
